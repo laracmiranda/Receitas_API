fb75d1e8abc336d45bb02abbdc0c0fbe7dcdc074
Lara Corsini <laracorsinidemiranda@gmail.com>
2025-09-07 20:14:18 -0300

    chore: adicionando mais rotas para a página do usuário

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,7 @@ import { UserController } from "../controllers/UserController.js";
 import authenticate from "../middlewares/authenticate.js";
 import { RecipeController } from "../controllers/RecipeController.js";
 import { FavoriteController } from "../controllers/FavoriteController.js";
+import { upload } from "../middlewares/upload.js";
 
 const router = Router();
 const userController = new UserController();
@@ -14,14 +15,21 @@ router.get("/", userController.findAllUsers);
 router.get("/me", authenticate, userController.findUserAuthenticated);
 router.get("/:id", userController.findUser);
 router.post("/", userController.createUser);
+
+// Rotas de Segurança e Conta
+router.put("/me/change-password", authenticate, userController.changePassword);
 router.put("/me", authenticate, userController.updateUser);
 router.delete("/me", authenticate, userController.deleteUser);
 
-// Rotas de Segurança
-router.put("/me/change-password", authenticate, userController.changePassword);
-
-// Rotas do Usuário
+// Recursos do Usuário
+// Receitas
 router.get("/me/recipes", authenticate, recipeController.findRecipeByUser);
+router.put("/me/recipes/:id", authenticate, upload.single("image"), recipeController.updateRecipe);
+router.delete("/me/recipes/:id", authenticate, recipeController.deleteRecipe);
+
+// Favoritos
 router.get("/me/favorites", authenticate, favoriteController.findFavorites);
+router.delete("/me/favorites/:recipeId", authenticate, favoriteController.deleteFavorite);
+
 
 export default router;
