0bee56679ce712b475d840f09af32941cb2b62f3
Lara Corsini <laracorsinidemiranda@gmail.com>
2025-09-30 20:15:31 -0300

    feat: add formatRecipe to standardize recipe json return

---
+++ utils/formatRecipe.js
@@ -0,0 +1,21 @@
+// Padronizar e melhorar o retorno na requisição das receitas
+import { formatPrepTime } from "./formatTime.js";
+
+export function formatRecipe(recipe) {
+  return {
+    id: recipe.id,
+    name: recipe.name,
+    category: recipe.category,
+    description: recipe.description,
+    ingredients: recipe.ingredients,
+    steps: recipe.steps,
+    prepTime: formatPrepTime(recipe.prepTime),
+    difficulty: recipe.difficulty,
+    portions: recipe.portions,
+    status: recipe.status,
+    creationDate: recipe.creationDate,
+    image: recipe.image,
+    user: recipe.user,
+    likes: recipe._count?.favorites ?? 0,
+  };
+}
